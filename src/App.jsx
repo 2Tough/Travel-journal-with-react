8ae603604196e18413f989427bde82cd5764b3eb
2Tough <vainferro@gmail.com>
2022-11-21 20:26:43 +0300

    Created new component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Card from './Card'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <>
     <h1>hello world</h1>
+    <Card />
+    </>
   )
 }
 
---
+++ src/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export default function Card() {
+    return (
+        <div>
+            <h1>text</h1>
+
+        </div>
+    )
+}
