3e0eabe1f75be8c1c3e3508bad49110c8afc309a
2Tough <vainferro@gmail.com>
2022-11-21 22:11:45 +0300

    Updated cards elements in components and main.

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,29 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Card from './Card'
+import data from './data'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const cards = data.map(item => {
+    return (
+      <Card 
+      key={item.id}
+      item={item}
+    
+    />
+    )
+  }
+    
+    
+    )
+
   return (
     <>
-    <h1>hello world</h1>
-    <Card />
+    <Navbar />
+    {cards}
+
     </>
   )
 }
