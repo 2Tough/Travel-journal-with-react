8de24133db9ae686260980ee2e8d22fa40234bfa
2Tough <vainferro@gmail.com>
2022-11-22 22:10:25 +0300

    Added basic structure.

--- src/Card.jsx
+++ src/Card.jsx
@@ -5,9 +5,12 @@ export default function Card(props) {
     return (
         <div className='card'>
             
-            <p>{props.item.location}</p>
             
-
+            <p>{props.item.location}</p>
+            <p>{props.item.googleMapsUrl}</p>
+            <h3>{props.item.title}</h3>
+            <span>{props.item.startDate} - {props.item.startDate}</span>
+            <p>{props.item.description}</p>
         </div>
     )
 }
